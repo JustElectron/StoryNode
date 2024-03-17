383f6a3bd3be911cfed8a3add16e3c3185098ad8
Andreas Ronning <and_just1@hotmail.com>
2024-03-17 21:58:14 +0100

    Restructured docker build. Added swagger document.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule} from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const file = readFileSync(join(process.cwd(), 'openapi.yaml')).toString('utf-8');
+  const document = parse(file);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
