74d9b3b56d08148f7256dd905f321d63a0843ee3
Andreas Ronning <and_just1@hotmail.com>
2024-08-27 20:53:02 +0200

    Fixed tests. Added correct providers with mocked values so tests can run.

--- backend/src/storyline/storyline.controller.spec.ts
+++ backend/src/storyline/storyline.controller.spec.ts
@@ -2,16 +2,38 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StorylineController } from './storyline.controller';
 import { StorylineService } from './storyline.service';
 
+const id = 1;
+const title = "Awsome Story";
+const description = "This is the awsome story of how i created this app";
+const storyLine1 = {
+  id: id,
+  title: title,
+  description: description,
+};
+
 describe('StorylineController', () => {
   let controller: StorylineController;
+  let service: StorylineService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [StorylineController],
-      providers: [StorylineService],
+      providers: [
+        {
+          provide: StorylineService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(storyLine1),
+            update: jest.fn().mockResolvedValue(storyLine1),
+            findOne: jest.fn().mockResolvedValue(storyLine1),
+            findAll: jest.fn().mockResolvedValue([storyLine1]),
+            remove: jest.fn().mockResolvedValue(null),
+          }
+        }
+      ],
     }).compile();
 
     controller = module.get<StorylineController>(StorylineController);
+    service = module.get<StorylineService>(StorylineService);
   });
 
   it('should be defined', () => {
--- backend/src/storyline/storyline.service.spec.ts
+++ backend/src/storyline/storyline.service.spec.ts
@@ -1,15 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StorylineService } from './storyline.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Storyline } from './entities/storyline.entity';
+import { Repository } from 'typeorm';
+
+const id = 1;
+const title = "Awsome Story";
+const description = "This is the awsome story of how i created this app";
+const storyLine1 = {
+  id: id,
+  title: title,
+  description: description,
+};
 
 describe('StorylineService', () => {
   let service: StorylineService;
+  let repo: Repository<Storyline>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StorylineService],
+      providers: [
+        StorylineService,
+        {
+          provide: getRepositoryToken(Storyline),
+          useValue: {
+            create: jest.fn().mockResolvedValue(storyLine1),
+            update: jest.fn().mockResolvedValue(storyLine1),
+            findOne: jest.fn().mockResolvedValue(storyLine1),
+            findAll: jest.fn().mockResolvedValue([storyLine1]),
+            remove: jest.fn().mockResolvedValue(null),
+          }
+        }
+      ],
     }).compile();
 
     service = module.get<StorylineService>(StorylineService);
+    repo = module.get<Repository<Storyline>>(getRepositoryToken(Storyline));
   });
 
   it('should be defined', () => {
