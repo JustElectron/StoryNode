74ed26d6e473feff1d0983527a879a17a360f9bb
Andreas Ronning <and_just1@hotmail.com>
2024-09-24 21:07:03 +0200

    Finished storynode service

--- backend/src/storynode/storynode.service.ts
+++ backend/src/storynode/storynode.service.ts
@@ -10,35 +10,52 @@ export class StorynodeService {
   constructor(
     @InjectRepository(StoryNode) private storyNodeRepository: Repository<StoryNode>
   ) {}
+  updateNodeRelations(storyNodeDTO: CreateStorynodeDto | UpdateStorynodeDto, thisNode: StoryNode) {
+    if (storyNodeDTO.nextNodeId) {
+      this.storyNodeRepository.findOneBy({id: storyNodeDTO.nextNodeId})
+        .then((nextNode) => {this.storyNodeRepository.update(nextNode.id, {prevNodeId: thisNode.id})})
+    }
+    if (storyNodeDTO.prevNodeId) {
+      this.storyNodeRepository.findOneBy({id: storyNodeDTO.prevNodeId})
+        .then((prevNode) => {this.storyNodeRepository.update(prevNode.id, {nextNodeId: thisNode.id})})
+    }
+  }
+
   create(createStorynodeDto: CreateStorynodeDto) {
     const newStoryNode = this.storyNodeRepository.create(createStorynodeDto);
     return this.storyNodeRepository.save(newStoryNode)
       .then((newNode) => {
-        if (createStorynodeDto.nextNodeId) {
-          this.storyNodeRepository.findOneBy({id: createStorynodeDto.nextNodeId})
-            .then((nextNode) => {this.storyNodeRepository.update(nextNode.id, {prevNodeId: newNode.id})})
-        }
-        if (createStorynodeDto.prevNodeId) {
-          this.storyNodeRepository.findOneBy({id: createStorynodeDto.prevNodeId})
-            .then((prevNode) => {this.storyNodeRepository.update(prevNode.id, {nextNodeId: newNode.id})})
-        }
-        return newNode
+        this.updateNodeRelations(createStorynodeDto,newNode)
+        return newNode;
       });
   }
 
   findAll() {
-    return this.storyNodeRepository.find({relations: {prevNode: true, nextNode: true}})
+    return this.storyNodeRepository.find({relations: {prevNode: true, nextNode: true}});
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} storynode`;
+    return this.storyNodeRepository.findOneBy({id: id});
   }
 
   update(id: number, updateStorynodeDto: UpdateStorynodeDto) {
-    return `This action updates a #${id} storynode`;
+    return this.storyNodeRepository.update(id, updateStorynodeDto)
+      .then(() => {
+        return this.findOne(id)
+          .then((thisNode) => {
+            this.updateNodeRelations(updateStorynodeDto, thisNode);
+            return thisNode;
+          });
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} storynode`;
+    return this.findOne(id)
+      .then ((val) => {
+        if (!val) {
+          return val;
+        }
+        return this.storyNodeRepository.remove(val)
+      })
   }
 }
